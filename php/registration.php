f20df6573794cbd663c5880c6de87fab11e7d0c1
Bekzhan111 <68604058+Bekzhan111@users.noreply.github.com>
2021-03-01 00:51:37 +0600

    Create registration.php

---
+++ php/registration.php
@@ -0,0 +1,20 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "ESP32");
+if(isset($_POST['register'])){
+$name= $_POST['name'];
+$surname=$_POST['surname'];
+$email=$_POST['email'];
+$password=$_POST['password'];
+$phoneNumber=$_POST['phoneNumber'];
+$address=$_POST['address'];
+     $select='SELECT * FROM `patients` WHERE `email`= "'.$email.'" ';
+     $result = mysqli_query($con,$select);
+     if($result){
+         if(mysqli_num_rows($result)>0){
+            echo "Пользователь с таким адресом электронной почты уже существует!";
+         }else{
+    $insert="INSERT INTO `users` (`id`, `name`, `surname`, `email`, `password`,`phoneNumber`, `address`) VALUES (NULL, '".$name."', '".$surname."', '".$email."', '".$password."','".$phoneNumber."', '".$address."')";
+$r=mysqli_query($con, $insert);
+             }
+}
+?>
