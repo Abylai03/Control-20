2e17035b62ecf55582846b45736a0a8091d66282
Abylay03 <bite2099@gmail.com>
2021-03-11 04:39:13 +0600

    Update doctorProfile.php

--- php/doctor_scripts/doctorProfile.php
+++ php/doctor_scripts/doctorProfile.php
@@ -9,7 +9,7 @@ if (!$_SESSION['doctor']) {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Профиль</title>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
