dcd76c85d739702e91facf272c2a2d00629161f8
Abylay03 <bite2099@gmail.com>
2021-08-13 17:56:41 +0600

    Update tables.php

--- php/doctor_scripts/tables.php
+++ php/doctor_scripts/tables.php
@@ -147,11 +147,11 @@ if (!$_SESSION['doctor']) {
 															</td>
 														</tr>
 												<?php
-													}
+													}/* 
 												} else {
 													echo "База пуста";
 												}
-
+ */
 												?>
 												</tbody>
 											</table>
